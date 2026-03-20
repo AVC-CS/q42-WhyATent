576b6dd31df5b31d627b335c230ea60b540454b3
WhyATent <WWyatnt@gmail.com>
2026-03-19 22:35:31 -0700

    q42 Done.

--- main.cpp
+++ main.cpp
@@ -13,9 +13,29 @@ int main()
 
     cout << "Enter the package weight and distance: ";
     cin >> weight >> distance;
-
-    // TODO
-
+    if (weight <= 0 or weight > 20){
+        cout << "Invalid weight. Input a weight between 1 and 20." << endl;
+    } else if (weight > 0 and weight <=20){
+        if (weight <= 2){
+            rate = rate1;
+        }
+        else if (weight <= 6){
+            rate = rate2;
+        }
+        else if (weight <= 10){
+            rate = rate3;
+        }
+        else if (weight <= 20){
+            rate = rate4;
+        }
+    }
+    if (distance < 10 or distance > 3000){
+        cout << "Invalid distance. Input a weight between 10 and 3000." << endl;
+    } else if (distance < 500){
+        total_charge = rate;
+    } else {
+        total_charge = (distance/500)*rate;
+    }
     cout << setw(10) << left << setprecision(2) << fixed;
     cout << total_charge << endl;
 
